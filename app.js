48d11d0927fa4a12ecceb0ef7dd464e417db2cfe
Alberto Bonacina <bonacina.alberto@gmail.com>
2017-12-29 16:14:49 +0100

    Added coinName and fullName to the info and save them to a JSON file

--- app.js
+++ app.js
@@ -23,6 +23,8 @@ fs.readFile('coinlist.json', 'utf8', function (err, data) {
         var thisObj = dataCoins[key];
         //Replace the * char in the currency's symbol (I don't know why some currency has got the * char in the symbol)
         var symbol = (thisObj.Symbol).replace(/\*/g,'');
+        var coinName = thisObj.CoinName;
+        var fullName = thisObj.FullName;
 
         if(thisObj.hasOwnProperty('ImageUrl')){  		
         
@@ -37,11 +39,15 @@ fs.readFile('coinlist.json', 'utf8', function (err, data) {
         		objThisValuta.symbol = symbol;
         		objThisValuta.ext = ext;
         		objThisValuta.url = coinImgeUrl;
+            objThisValuta.coin_name = coinName;
+            objThisValuta.full_name = fullName;
 	        arrayCurrencies.push(objThisValuta);
 
        	}
     });
 
+    //This is the string that I want to save as json for later use
+    var jsonObjectToSave = '{"currencies":[';
     async.each(arrayCurrencies,
       function(item, callback){
         
@@ -49,10 +55,25 @@ fs.readFile('coinlist.json', 'utf8', function (err, data) {
         //Save the cryptocurrency's image in the images dir
         exec('/usr/bin/wget -c '+item.url +' -O ./images/'+item.symbol+'.'+item.ext);
         
+        jsonObjectToSave += '{"coin_name": "' + item.coin_name + '",'+
+                            '"full_name":"' + item.full_name+ '",'+
+                            '"path":"images/' + item.symbol+ '.'+ item.ext + '",'+
+                            '"symbol":"' + item.symbol + '"},';    
+
         callback();
       },
       function(err){
-        console.log("Done!");
+
+        jsonObjectToSave = jsonObjectToSave.substring(0,jsonObjectToSave.length-1);
+        jsonObjectToSave += ']}';
+
+        fs.writeFile("organized-cryptocurrency.json",jsonObjectToSave,function(err) {
+            if(err) {
+                return console.log(err);
+            }
+            console.log("File Saved - Done!");
+        });
+
       }
     );
 
